3060390fe68b07a4a1f5a18eae9a421affd72e2a
Mohamed Hazem <125741147+mohamed-hazem-fathy@users.noreply.github.com>
2024-03-16 22:43:55 +0200

    last

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <v-skeleton-loader type="image" width="130px" height="130px"> </v-skeleton-loader>
+
+
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["vuetify/styles/main.sass", "@mdi/font/css/materialdesignicons.css"],
+  build: {
+    transpile: ["vuetify"],
+  }
+
+})
